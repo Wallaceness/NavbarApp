7bf5637df95e0f4f74a4ac4d0bf2be322bb51a03
Wallaceness <ncw232@gmail.com>
2020-02-08 21:57:06 -0500

    Navigation set up and running!

--- app/src/main/java/com/example/android/navigationbarapp/MainActivity.java
+++ app/src/main/java/com/example/android/navigationbarapp/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.PersistableBundle;
 import android.view.View;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private AlphabetFragment alphabetView;
     private TapperMainFragment tapperView;
     private Birthstones_main birthStonesView;
+    private NavHostFragment navHost;
     private Toolbar toolbar;
     private int currentFragment;
 
@@ -34,19 +36,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mainContainer = findViewById(R.id.container_div);
+//        mainContainer = findViewById(R.id.container_div);
 
-        alphabetView = new AlphabetFragment();
-        tapperView = new TapperMainFragment();
-        birthStonesView = new Birthstones_main();
-        FragmentTransaction transaction = MainManager.beginTransaction();
-        if (savedInstanceState!=null){
-//            transaction.add(R.id.container_div, MainManager.findFragmentById(savedInstanceState.getInt("current_fragment"))).addToBackStack(null).commit();
-        }
-        else{
-            transaction.add(R.id.container_div, alphabetView).addToBackStack(null).commit();
-            currentFragment = alphabetView.getId();
-        }
+//        alphabetView = new AlphabetFragment();
+//        tapperView = new TapperMainFragment();
+//        birthStonesView = new Birthstones_main();
+        navHost = (NavHostFragment) MainManager.findFragmentById(R.id.nav_host_fragment);
+//        FragmentTransaction transaction = MainManager.beginTransaction();
+//        if (savedInstanceState!=null){
+////            transaction.add(R.id.container_div, MainManager.findFragmentById(savedInstanceState.getInt("current_fragment"))).addToBackStack(null).commit();
+//        }
+//        else{
+//            transaction.add(R.id.container_div, alphabetView).addToBackStack(null).commit();
+//            currentFragment = alphabetView.getId();
+//        }
     }
 
     @Override
@@ -64,27 +67,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.alphabet) {
-            FragmentTransaction transaction1 = MainManager.beginTransaction();
-            transaction1.replace(R.id.container_div, alphabetView).addToBackStack(null).commit();
-            currentFragment = alphabetView.getId();
-            toolbar.setTitle(item.getTitle());
-            return true;
-        }
-        else if (id==R.id.stress_tapper){
-            FragmentTransaction transaction2 = MainManager.beginTransaction();
-            transaction2.replace(R.id.container_div, tapperView).addToBackStack(null).commit();
-            currentFragment= tapperView.getId();
-            toolbar.setTitle(item.getTitle());
-            return true;
-        }
-        else if (id==R.id.Birthstones){
-            FragmentTransaction transaction3 = MainManager.beginTransaction();
-            transaction3.replace(R.id.container_div, birthStonesView).addToBackStack(null).commit();
-            currentFragment = birthStonesView.getId();
-            toolbar.setTitle(item.getTitle());
-            return true;
-        }
+
+        toolbar.setTitle(item.getTitle());
+        NavHostFragment.findNavController(navHost).navigate(id);
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/example/android/navigationbarapp/MainAdapter.java
+++ app/src/main/java/com/example/android/navigationbarapp/MainAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.navigationbarapp;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +9,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
